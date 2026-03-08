8415dcacf6477585566f12299042c018e9022bc1
Nick Nikolakakis <nonicked@protonmail.com>
2026-03-08 21:24:25 +0200

    chore: regenerate metadata after rebase
    Signed-off-by: Nick Nikolakakis <nonicked@protonmail.com>

--- receiver/sqlserverreceiver/internal/metadata/generated_config.go
+++ receiver/sqlserverreceiver/internal/metadata/generated_config.go
@@ -54,11 +54,6 @@ func (ms *MetricConfig) Unmarshal(parser *confmap.Conf) error {
 	return nil
 }
 
-// AttributeConfig holds configuration information for a particular metric.
-type AttributeConfig struct {
-	Enabled bool `mapstructure:"enabled"`
-}
-
 // MetricsConfig provides config for sqlserver metrics.
 type MetricsConfig struct {
 	SqlserverBatchRequestRate                   MetricConfig `mapstructure:"sqlserver.batch.request.rate"`
